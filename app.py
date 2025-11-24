88de17c5b6632442fdee0f7047b1c5c1bd3ff9f8
Abdulnasir Williams <aaw20@usf.edu>
2025-11-23 21:58:43 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+# created a Welcome message and asked user for input on hours worked
 print('Welcome to my Python Program!')
 hour = input("How many hours did you work this week?")
+# Converted input to float and calculated monthly hours
 hours = float(hour)
 monthly_hours = hours * 4
+# Printed out the total monthly hours that the user is on track to work
 print(f' You are on track to work {monthly_hours} hours this month!')
-try:
+# Created a simple error handling if a valid number is not entered
+ try:
     hours = float
 except ValueError:
     print("Please enter a valid number")
