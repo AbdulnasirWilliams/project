3062a44ffd8f4e9d20d578c0989e0ee4e43793cc
Abdulnasir Williams <aaw20@usf.edu>
2025-11-23 21:54:37 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ print('Welcome to my Python Program!')
 hour = input("How many hours did you work this week?")
 hours = float(hour)
 monthly_hours = hours * 4
+print(f' You are on track to work {monthly_hours} hours this month!')
